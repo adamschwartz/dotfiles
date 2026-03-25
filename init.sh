9cf18d8992d2e25070ec12d5ba6a8a42df3afb76
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2026-03-25 16:11:51 -0700

    initialize init.sh ¬‿¬

---
+++ init.sh
@@ -0,0 +1,7 @@
+# Run this file one time when setting up new machines
+
+# Install ffmpeg
+brew install ffmpeg
+
+# Make `main` the default branch for new repos
+git config --global init.defaultBranch main
